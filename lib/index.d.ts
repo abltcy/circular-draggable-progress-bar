cd2812b61404b842e94726bf8ed1dfe5fc77a52b
abltcy <bilgi@ahmetbaltaci.com>
2021-12-02 15:42:29 +0000

    npm lib

---
+++ lib/index.d.ts
@@ -0,0 +1,4 @@
+import { ICircularDraggableProgressBar } from './types';
+export declare const CircularDraggableProgressBar: ({ strokeWidth, radius, iconOutSideColor, gradientColorFrom, gradientColorTo, bgCircleColor, draggable, text, max, value, displayNumber, onChange, handlerPanResponder, symbol, symbolPosition, icon: Icon, individualValue, }: ICircularDraggableProgressBar) => JSX.Element;
+declare const _default: any;
+export default _default;
